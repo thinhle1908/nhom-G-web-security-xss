43bd9e949f930b469aa7cd78731dca85326d4bdb
Lê Hoàng Đức Thịnh <86552685+thinhle1908@users.noreply.github.com>
2022-10-21 16:37:56 +0700

    them 2 cach khac phuc xss

--- models/UserModel.php
+++ models/UserModel.php
@@ -65,8 +65,9 @@ class UserModel extends BaseModel {
      * @return mixed
      */
     public function insertUser($input) {
+        $name =  htmlspecialchars( $input['name']) ;
         $sql = "INSERT INTO `app_web1`.`users` (`name`, `password`) VALUES (" .
-                "'" . $input['name'] . "', '".md5($input['password'])."')";
+                "'" . $name . "', '".md5($input['password'])."')";
 
         $user = $this->insert($sql);
 
